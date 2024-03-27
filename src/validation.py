ca8e747e807fec4ef25620b1214f9ab634408e20
root <root@fowler-lpw-dgx-0.chapman.edu>
2024-03-27 21:00:51 +0000

    Adding finished validation to github

--- src/validation.py
+++ src/validation.py
@@ -1,6 +1,8 @@
 from tensorflow.keras.models import Model
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow as tf
 #from GradCAM import GradCAM
 #import cv2
@@ -9,7 +11,85 @@ def evaluate_model(model, dataset):
     loss, accuracy = model.evaluate(dataset)
     print(f'Test Loss: {loss}')
     print(f'Test Accuracy: {accuracy}')
+    
+    TP_list = []
+    TN_list = []
+    FP_list = []
+    FN_list = []
 
+    for batch in dataset.take(30):
+        x_batch, y_true_batch = batch
+        y_pred_batch = model.predict(x_batch)
+        
+        for i in range(len(x_batch)):
+            y_true = y_true_batch[i].numpy()
+            y_pred = (y_pred_batch[i] > 0.5).astype(int)
+
+            TP_list.append(np.sum((y_true == 1) & (y_pred == 1)))
+            TN_list.append(np.sum((y_true == 0) & (y_pred == 0)))
+            FP_list.append(np.sum((y_true == 0) & (y_pred == 1)))
+            FN_list.append(np.sum((y_true == 1) & (y_pred == 0)))
+
+    TP_avg = np.mean(TP_list)
+    TN_avg = np.mean(TN_list)
+    FP_avg = np.mean(FP_list)
+    FN_avg = np.mean(FN_list)
+
+    precision_avg = TP_avg / (TP_avg + FP_avg) if (TP_avg + FP_avg) > 0 else 0
+    recall_avg = TP_avg / (TP_avg + FN_avg) if (TP_avg + FN_avg) > 0 else 0
+    f1_score_avg = 2 * (precision_avg * recall_avg) / (precision_avg + recall_avg) if (precision_avg + recall_avg) > 0 else 0
+    
+    array = [TN_avg, FP_avg, FN_avg, TP_avg, loss, accuracy, precision_avg, recall_avg, f1_score_avg]
+    return array
+
+def display_scores(array):
+    TN_avg = array[0]
+    FP_avg = array[1]
+    FN_avg = array[2]
+    TP_avg = array[3]
+    loss = array[4]
+    accuracy = array[5]
+    precision_avg = array[6]
+    recall_avg = array[7]
+    f1_score_avg = array[8]
+    
+    
+    print(f'Test Loss: {loss}')
+    print(f'Test Accuracy: {accuracy}')
+    print("Average Precision:", precision_avg)
+    print("Average Recall:", recall_avg)
+    print("Average F1 Score:", f1_score_avg)
+
+    confusion_matrix_avg = np.array([[TP_avg, FN_avg], [FP_avg, TN_avg]])
+
+    fig, ax = plt.subplots()
+    sns.heatmap(confusion_matrix_avg, annot=True, fmt='.2f', cmap='Blues', ax=ax)
+
+    label_names = ['Positive', 'Negative']
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Average Confusion Matrix')
+    ax.xaxis.set_ticklabels(label_names)
+    ax.yaxis.set_ticklabels(label_names, rotation=0)
+    plt.show()
+
+    total_avg = TN_avg + FP_avg + FN_avg + TP_avg
+    confusion_matrix_fraction = np.array([[TP_avg/total_avg, FN_avg/total_avg], [FP_avg/total_avg, TN_avg/total_avg]])
+
+    fig, ax = plt.subplots()
+    sns.heatmap(confusion_matrix_fraction, annot=True, fmt='.2f', cmap='Blues', ax=ax)
+
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Decimal Average Confusion Matrix')
+    ax.xaxis.set_ticklabels(label_names)
+    ax.yaxis.set_ticklabels(label_names, rotation=0)
+    plt.show()
+
+def predict(model, dataset):
+    predictions = model.predict(dataset)
+    predictions = predictions > 0.5
+    return predictions
 
 def three_best_three_worst_predictions(model, dataset):
     differences = []
@@ -21,8 +101,31 @@ def three_best_three_worst_predictions(model, dataset):
             x = x_batch[i]
             y_true = y_true_batch[i]
             y_pred = y_pred_batch[i]
+            print("y_true", type(y_true))
+            print("y_pred", type(y_pred))
+            
+            y_true_np = y_true.numpy()
+            y_pred = y_pred > 0.5
+
+            TP = np.sum((y_true_np == 1) & (y_pred == 1))
+            TN = np.sum((y_true_np == 0) & (y_pred == 0))
+            FP = np.sum((y_true_np == 0) & (y_pred == 1))
+            FN = np.sum((y_true_np == 1) & (y_pred == 0))
+            print("TP: ", TP)
+            print("TN: ", TN)
+            print("FP: ", FP)
+            print("FN: ", FN)
+            precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+            recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+            f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+            print("Precision:", precision)
+            print("Recall:", recall)
+            print("F1 Score:", f1_score)
             difference = np.abs(y_pred - y_true.numpy())
             differences.append((x, y_true, y_pred, np.sum(difference)))
+            
+            return None
 
     differences.sort(key=lambda x: x[3])
     best_predictions = differences[:3]  # Take the 3 best predictions
@@ -48,7 +151,7 @@ def apply_gradcam(img, model, gradcam, layerName=None):
     plt.show()
 
 def validation(model, test_dataset, layerName=None):
-    evaluate_model(model, test_dataset)
+    return evaluate_model(model, test_dataset)
     '''
     # Initialize GradCAM
     if layerName is None:
